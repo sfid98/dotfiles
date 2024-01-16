2fb195566839f42eaf054bc05b84422a0dbec76c
sfid98 <stanifidanza98@gmail.com>
2024-01-16 12:31:03 +0100

    update

--- batteryNotification.sh
+++ batteryNotification.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 while true; do
-	bat_lvl=$(cat /sys/class/power_supply/macsmc-battery/capacity) 
-  if [ "$bat_lvl" -le 20 ]; then
-    notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
-    sleep 120
-  else
-    sleep 100
-  fi
+    bat_lvl=$(cat /sys/class/power_supply/macsmc-battery/capacity)
+    chargingState=$(upower -i /org/freedesktop/UPower/devices/battery_macsmc_battery | grep -E "state" | awk 'BEGIN { FS=":" } {print $2}' | sed 's/ //g')
+    if [ "$bat_lvl" -le 20 ] && [ "$chargingState" != "charging" ]; then
+        notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
+        sleep 120
+    else
+        sleep 100
+    fi
 done
+
